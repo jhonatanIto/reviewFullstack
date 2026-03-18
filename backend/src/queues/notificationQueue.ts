0998f38634a7960efd8b77628e11e2b9a483445e
Jhonatan <jhonatan-ito@hotmail.com>
2026-03-18 11:01:15 +0900

    route fix

--- backend/src/queues/notificationQueue.ts
+++ backend/src/queues/notificationQueue.ts
@@ -4,5 +4,6 @@ export const notificationQueue = new Queue("notifications", {
   connection: {
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: Number(process.env.REDIS_PORT) || 6379,
+    password: process.env.REDIS_PASSWORD,
   },
 });
